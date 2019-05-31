c16e7821a1122205be41bfc12a4c5241065cd48f
Grenier Célestin <cgrenier003@ensc.fr>
2019-05-31 19:55:04 +0200

    Changed receiver
    * switch receiver to `ACTION_USER_PRESENT` (from `ACTION_SCREEN_OFF`)
        -> prevent reload from rapid screen on / screen off
        -> higher loading time after unlock

--- app/src/main/java/com/patatos/sac/anoca/Receive.kt
+++ app/src/main/java/com/patatos/sac/anoca/Receive.kt
@@ -3,11 +3,14 @@ package com.patatos.sac.anoca
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 
 class Receive : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        if (intent?.action == Intent.ACTION_SCREEN_OFF) {
+        Log.i("anoca::receiver", "Received ${intent?.action}")
+
+        if (intent?.action == Intent.ACTION_USER_PRESENT) {
             val activity = Intent(context, MainActivity::class.java)
             activity.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
             context!!.startActivity(activity)
